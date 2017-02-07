f357bcf58bb2d25f29895be40207b75efa2266f1
zhi <zhl108@ucsd.edu>
2017-02-07 10:12:04 -0800

    reg file completed

--- processor/sv/reg_file.sv
+++ processor/sv/reg_file.sv
@@ -1,41 +1,30 @@
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:24:09 10/27/2011 
-// Design Name: 
-// Module Name:    reg_file 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-
-module reg_file #(parameter W=16, D=4)(
+module reg_file #(parameter W=8, D=4)(
     input         CLK,
 	              RegWrite,
-    input  [ D-1:0] srcA,
-                    srcB,
-                    writeReg,
+                  AccWrite,
+    input  [ D-1:0] reg_index,
     input  [ W-1:0] writeValue,
-    output [ W-1:0] ReadA,
-    output logic [W-1:0] ReadB
+
+    output [ W-1:0] Acc_out,
+    output logic [W-1:0] Reg_out
     );
 
-// 32 bits wide [31:0] and 64 registers deep [0:63] or just [64]	 
+// 8 bits wide [7:0] and 16 registers deep [0:15] or just [16]	 
 logic [W-1:0] registers[2**D];
 
 // combinational reads
-assign      ReadA = (srcA == 'b0)? 'b0 : registers[srcA];
-always_comb ReadB = (srcB == 'b0)? 'b0 : registers[srcB];
+assign Acc_out = registers[0];              //accumulator is the first register in RF in our design by default
+assign Reg_out = registers[reg_index];
 
 // sequential (clocked) writes
-always_ff @ (posedge CLK)
-  if (RegWrite && (writeReg != 'b0))
-    registers[writeReg] <= writeValue;
-
+always_ff@(posedge CLK)
+begin
+    if(AccWrite)
+        registers[0] <= writeValue;
+    else
+    begin
+        if(RegWrite)
+            registers[reg_index] <= writeValue;
+    end
+end
 endmodule
